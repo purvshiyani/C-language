3c46376c307b0a8766a12ba97079620c8631eb71
purvshiyani <kiranbensiyani@gmail.com>
2023-05-01 17:40:28 +0530

    Ch 1: Introduction to C Language

---
+++ ch1/P-S-P.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("name\t\t : Purv shiyani\n");
+	printf("course\t\t : c language\n");
+	printf("Age\t\t : 16 \n");
+	printf("Contect mo\t : 12345678\n");
+	printf("Hobby\t\t :coding\n");
+	printf("school\t\t : L.P.S\n");
+	printf("city\t\t : surat\n");
+	printf("fevorate animal\t : dog\n");
+	getch();
+}
---
+++ ch1/P-S-P2.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	printf("-------\n");
+	printf("|     |\n");
+	printf("p     |\n");
+	printf("u     |\n");
+	printf("r     |\n");
+	printf("v     |\n");
+	printf("|     |\n");
+	printf("-------\n");
+	getch();
+}
